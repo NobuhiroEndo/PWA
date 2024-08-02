1fa52e650601881677c1035157e06b3370ca4c75
NobuhiroEndo <nobuhiro.0320@gmail.com>
2024-08-02 18:07:10 +0000

    heroku

--- mobile/src/app/utils/getVapidPublicKey.js
+++ mobile/src/app/utils/getVapidPublicKey.js
@@ -1,21 +1,6 @@
-const clickVisitSiteButton = () => {
-  // ボタンが存在するかどうかを確認
-  const button = document.querySelector('button#visit-site');
-  if (button) {
-    button.click();
-  } else {
-    console.log('「Visit Site」ボタンが見つかりません');
-  }
-};
-
 const getVapidPublicKey = async () => {
   const baseURL = process.env.REACT_APP_API_BASE_URL;
   console.log(`Fetching VAPID public key from: ${baseURL}/get_vapid_public_key/`);
-  // 初回のアクセスでボタンが表示されているか確認
-  clickVisitSiteButton();
-
-  // 1秒待ってからAPIリクエストを送信
-  await new Promise(resolve => setTimeout(resolve, 1000));
   
   try {
     const res = await fetch(`${baseURL}/get_vapid_public_key/`);
