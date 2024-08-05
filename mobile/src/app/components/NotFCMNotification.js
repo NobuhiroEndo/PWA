b1958f4e9786e9e10ff1fb7caaa33dc78a801f8f
NobuhiroEndo <nobuhiro.0320@gmail.com>
2024-08-06 00:20:00 +0900

    マルチログイン

--- mobile/src/app/components/NotFCMNotification.js
+++ mobile/src/app/components/NotFCMNotification.js
@@ -1,23 +1,31 @@
-import React, { useState, useEffect } from "react";
+// components/NotFCMNotification.js
+import React from 'react';
 import { subscribe } from "../utils/subscribe";
+import useAuth from '../hooks/useAuth'; 
 
 const NotFCMNotification = () => {
-  
+    const { authToken } = useAuth();
+
+    console.log('おーすとーくん：',authToken)
+
     const handleRequestToken = async () => {
       try {
-        await subscribe();
-        alert('プッシュ通知が正常に購読されました');
+        if (authToken) {
+            await subscribe(authToken);
+            alert('プッシュ通知が正常に購読されました');
+        } else {
+            console.error('認証トークンが見つかりません');
+        }
       } catch (error) {
         console.error('プッシュ通知の購読に失敗しました①', error);
-        // alert('プッシュ通知の購読に失敗しました②');
       }
     };
-  
+
     return (
       <div>
         <button onClick={handleRequestToken}>プッシュ通知を受け取る</button>
       </div>
     );
-  };
-  
-  export default NotFCMNotification;
+};
+
+export default NotFCMNotification;
--- mobile/src/app/main.js
+++ mobile/src/app/main.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Notification from '@/app/components/Notification';
-import NotFCMNotification from './components/NotFCMNotification';
-import { checkNotification } from './utils/checkNotification';
+import App from './app';
 
 // 'app-root' というIDを持つ要素を取得
 const container = document.getElementById('app-root');
 
 if (window.navigator.serviceWorker !== undefined) {
     window.navigator.serviceWorker.register('/serviceworker.js');
-  }
+}
 
 if (container) {
     // React 18 の createRoot メソッドを使用してルートを作成
     const root = ReactDOM.createRoot(container);
     
-    // Notification コンポーネントをレンダリング
-    root.render(
-        <React.StrictMode>
-            {/* <Notification /> */}
-            <NotFCMNotification />
-        </React.StrictMode>
-    );
+    // App コンポーネントをレンダリング
+    root.render(<App />);
 } else {
     console.error('Element with ID "app-root" not found.');
 }
